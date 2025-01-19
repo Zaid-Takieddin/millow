2ebfe7a7a3bb7f3c00f95778f68f9c6599595de3
Zaid-Takieddin <zaidtakieddin@gmail.com>
2025-01-19 17:40:55 +0400

    add inspection functionality to escrow contract with tests

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -19,7 +19,8 @@ contract Escrow {
     mapping(uint256 => uint256) public purchasePrice;
     mapping(uint256 => uint256) public escrowAmount;
     mapping(uint256 => address) public buyer;
-
+    mapping(uint256 => bool) public inspectionPassed;
+    
     modifier onlySeller() {
         require(msg.sender == seller, "Only seller can call this method");
         _;
@@ -27,6 +28,11 @@ contract Escrow {
 
     modifier onlyBuyer(uint256 _nftID) {
         require(msg.sender == buyer[_nftID], "Only buyer can call this method");
+        _;
+    }
+
+    modifier onlyInspector() {
+        require(msg.sender == inspector, "Only inspector can call this method");
         _;
     }
 
@@ -59,6 +65,13 @@ contract Escrow {
 
     function depositEarnest(uint256 _nftID) public payable onlyBuyer(_nftID) {
         require(msg.value >= escrowAmount[_nftID]);
+    }
+
+    function updateInspectionStatus(uint256 _nftID, bool _passed)
+        public
+        onlyInspector
+    {
+        inspectionPassed[_nftID] = _passed;
     }
 
     receive() external payable {}
