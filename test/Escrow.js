1747508227899e14b3bc67c69ba990e7dc413c87
Zaid-Takieddin <zaidtakieddin@gmail.com>
2025-01-19 17:11:04 +0400

    add list function to escrow contract with tests

--- test/Escrow.js
+++ test/Escrow.js
@@ -37,6 +37,12 @@ describe("Escrow", () => {
     // Approve Property
     transaction = await realEstate.connect(seller).approve(escrow.address, 1);
     await transaction.wait();
+
+    // List Property
+    transaction = await escrow
+      .connect(seller)
+      .list(1, buyer.address, tokens(10), tokens(5));
+    await transaction.wait();
   });
 
   describe("Deployment", () => {
@@ -58,6 +64,32 @@ describe("Escrow", () => {
     it("Returns lender", async () => {
       const result = await escrow.lender();
       expect(result).to.be.equal(lender.address);
+    });
+  });
+
+  describe("Listing", () => {
+    it("Updates as listed", async () => {
+      const result = await escrow.isListed(1);
+      expect(result).to.be.equal(true);
+    });
+
+    it("Returns buyer", async () => {
+      const result = await escrow.buyer(1);
+      expect(result).to.be.equal(buyer.address);
+    });
+
+    it("Returns purchase price", async () => {
+      const result = await escrow.purchasePrice(1);
+      expect(result).to.be.equal(tokens(10));
+    });
+
+    it("Returns escrow amount", async () => {
+      const result = await escrow.escrowAmount(1);
+      expect(result).to.be.equal(tokens(5));
+    });
+
+    it("Updates ownership", async () => {
+      expect(await realEstate.ownerOf(1)).to.be.equal(escrow.address);
     });
   });
 });
